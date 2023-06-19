6e32740a4fe78154f798d5e7545b9f9236883382
itonse <govl3dnjs@gmail.com>
2023-06-19 12:10:53 +0900

    create project

---
+++ src/main/java/com/itonse/clotheslink/ClotheslinkApplication.java
@@ -0,0 +1,13 @@
+package com.itonse.clotheslink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClotheslinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClotheslinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itonse/clotheslink/ClotheslinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itonse.clotheslink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClotheslinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
